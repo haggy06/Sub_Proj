4baaacf430825748d188d92c5b8cefd83bedad62
haggy06 <baegjonghyeog80@gmail.com>
2024-06-20 21:53:22 +0900

    파티클을 이용한 상호작용 제작

--- Assets/02Scripts/Interact/DetectionBase.cs
+++ Assets/02Scripts/Interact/DetectionBase.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public abstract class DetectionBase : MonoBehaviour
 {
     [SerializeField]
-    protected Tag targetTag = Tag.Player;
+    private bool isParticle = false;
+
     [SerializeField]
+    protected Tag targetTag = Tag.Player;
+
     protected bool detection = false;
     public bool Detection
     {
@@ -33,6 +36,22 @@ public abstract class DetectionBase : MonoBehaviour
     protected virtual void Awake()
     {
         gameObject.layer = (int)LAYER.Censor; // Censor 레이어는 Censor, Ground와만 충돌한다.
+
+        if (isParticle) // 파티클용일 땐 센서를 끈 상태로 시작
+        {
+            GetComponent<Collider2D>().enabled = false;
+        }
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.CompareTag(targetTag.ToString())) // 타겟과 충돌했을 경우
+        {
+            DetectionStart();
+        }        
+        else if (other.layer == (int)LAYER.Ground) // 땅과 충돌했을 경우
+        {
+            HitGround();
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -41,12 +60,9 @@ public abstract class DetectionBase : MonoBehaviour
         {
             HitGround();
         }
-        else
+        else if (collision.CompareTag(targetTag.ToString())) // 타겟과 충돌했을 경우
         {
-            if (collision.CompareTag(targetTag.ToString()))
-            {
-                Detection = true;
-            }
+            Detection = true;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
